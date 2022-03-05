beafb708d59366dca5665cf99c2c6ce981aefe70
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-03-05 18:52:33 +0900

    BOJ - 온풍기 안녕!(23289) - 수정중

---
+++ algorithm-problemSolving/implement/BOJ/Main_23289.java
@@ -0,0 +1,12 @@
+// BOJ - 온풍기 안녕!(23289)
+// 구현 - 수정중
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_23289 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
