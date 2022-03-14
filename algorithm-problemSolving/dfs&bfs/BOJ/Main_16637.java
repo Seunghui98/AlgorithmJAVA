d7b81b808dfc2a2372e0be538e898732517ca684
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-03-14 23:00:04 +0900

    BOJ - 괄호 추가하기(16637번) - DFS(백트래킹)

--- algorithm-problemSolving/dfs&bfs/BOJ/Main_16637.java
+++ algorithm-problemSolving/dfs&bfs/BOJ/Main_16637.java
@@ -1,75 +1,64 @@
+// BOJ - 괄호 추가하기(16637번)
+// DFS(백트래킹)
+// 삼성 A형 기출
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
- 
+
+
 public class Main_16637 {
-    static int ans;
-    static ArrayList<Character> ops;
-    static ArrayList<Integer> nums;
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static int n;
+
+    public static ArrayList<Integer> number;
+    public static ArrayList<Character> cal;
+
+    public static int ans;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        String input = br.readLine();
- 
-        ops = new ArrayList<>();
-        nums = new ArrayList<>();
- 
-        for (int i = 0; i < N; i++) {
-            char c = input.charAt(i);
-            if (c == '+' || c == '-' || c == '*') {
-                ops.add(c);
-                continue;
+        n = Integer.parseInt(br.readLine());
+        char[]input = br.readLine().toCharArray();
+
+        cal = new ArrayList<>();
+        number = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(input[i] == '*' || input[i] == '+' || input[i] == '-'){
+                cal.add(input[i]);
+            } else {
+                number.add(input[i] - '0');
             }
-            nums.add(Character.getNumericValue(c));
         }
- 
         ans = Integer.MIN_VALUE;
-        DFS(nums.get(0), 0);
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        dfs(number.get(0), 0);
+
+
+
+
+        System.out.println(ans);
     }
- 
-    // 연산
-    public static int calc(char op, int n1, int n2) {
-        switch (op) {
-        case '+':
-            return n1 + n2;
-        case '-':
-            return n1 - n2;
-        case '*':
-            return n1 * n2;
-        }
-        return -1;
+
+    public static void dfs(int result, int idx){
+       if(idx == cal.size()){
+           ans = Math.max(ans, result);
+           return;
+       }
+       dfs(cal(result, number.get(idx+1), cal.get(idx)), idx+1);
+
+       if(idx+2 <= cal.size()){
+           dfs(cal(result, cal(number.get(idx+1), number.get(idx+2),cal.get(idx+1)), cal.get(idx)),idx+2);
+
+       }
     }
- 
-    // DFS, 백트래킹 활용.
-    public static void DFS(int result, int opIdx) {
-        // 주어진 연산자의 개수를 초과하였을 경우.
-        if (opIdx >= ops.size()) {
-            ans = Math.max(ans, result);
-            return;
-        }
- 
-        // 괄호가 없는 경우
-        int res1 = calc(ops.get(opIdx), result, nums.get(opIdx + 1));
-        DFS(res1, opIdx + 1);
- 
-        // 괄호가 있는 경우
-        if (opIdx + 1 < ops.size()) {
-            // result의 오른쪽에 있는 값을 연산함.
-            int res2 = calc(ops.get(opIdx + 1), nums.get(opIdx + 1), nums.get(opIdx + 2));
- 
-            // 현재 result와 방금 구한 괄호 값을 연산한 결과와 괄호 오른쪽에 존재하는 연산자의 인덱스를 넘김.
-            DFS(calc(ops.get(opIdx), result, res2), opIdx + 2);
+
+
+
+    public static int cal(int a, int b, char cal_str){
+        if(cal_str == '*'){
+            return a*b;
+        } else if(cal_str == '-'){
+            return a-b;
+        } else {
+            return a+b;
         }
     }
- 
 }
