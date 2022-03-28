c3d8b4099dcae2aa972e23cf95acb310fdebdf37
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-03-29 00:42:44 +0900

    BOJ - 구슬 탈출 2 (13460번) - 구현 + BFS

--- algorithm-problemSolving/Samsung/Main_13460.java
+++ algorithm-problemSolving/Samsung/Main_13460.java
@@ -1,9 +1,11 @@
+// BOJ - 구슬 탈출 2 (13460번)
+// 구현 + BFS
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+
 
 public class Main_13460 {
 	public static int n, m;
@@ -35,7 +37,7 @@ public class Main_13460 {
 		m = Integer.parseInt(data[1]);
 		
 		map = new char[n][m];
-		int b_x, b_y, r_x, r_y;
+		int b_x = 0, b_y = 0, r_x = 0, r_y = 0;
 		for(int i=0;i<n;i++) {
 			map[i] = br.readLine().toCharArray();
 			for(int j=0;j<m;j++) {
@@ -49,7 +51,9 @@ public class Main_13460 {
 			}
 		}
 		
+		int ans = bfs(b_x, b_y, r_x, r_y);
 		
+		System.out.println(ans);
 		
 
 	}
@@ -63,12 +67,38 @@ public class Main_13460 {
 		
 		while (!q.isEmpty()) {
 			Data data = q.poll();
-			if(data.cnt > 10) {
+			if(data.cnt >= 10) {
 				return -1;
 			}
 			
 			for(int d=0;d<4;d++) {
-				int[] c = move(r_x, r_y, b_x, b_y);
+				int[] moving = move(data.r_x, data.r_y, data.b_x, data.b_y, d);
+				int rx = moving[0];
+				int ry = moving[1];
+				int bx = moving[2];
+				int by = moving[3];
+				int rcnt = moving[4];
+				int bcnt = moving[5];
+
+				if(map[bx][by] != 'O') {
+					if(map[rx][ry] == 'O') {
+						return data.cnt+1;
+					} 
+					
+					if(rx == bx && ry == by) {
+						if(rcnt > bcnt) {
+							rx -= dx[d];
+							ry -= dy[d];
+						} else {
+							bx -= dx[d];
+							by -= dy[d];
+						}
+					}
+					if(!visited[rx][ry][bx][by]) {
+						q.offer(new Data(rx, ry, bx, by, data.cnt+1));
+						visited[rx][ry][bx][by] = true;
+					}
+				} 
 			}
 			
 		}
@@ -78,8 +108,41 @@ public class Main_13460 {
 		return -1;
 	}
 	
-	public static int[] move(int r_x, int r_y, int b_x, int b_y) {
+	public static int[] move(int r_x, int r_y, int b_x, int b_y, int d) {
+		int r_cnt = 0;
 		
+		while (true) {			
+			r_x += dx[d];
+			r_y += dy[d];
+			if(map[r_x][r_y] == '#') {
+				r_x -= dx[d];
+				r_y -= dy[d];
+				break;
+				
+			}
+			r_cnt++;
+			if(map[r_x][r_y] == 'O') {
+				break;
+			}
+		}
+		
+		int b_cnt = 0;
+		while(true) {
+			b_x += dx[d];
+			b_y += dy[d];
+			if(map[b_x][b_y] == '#') {
+				b_x -= dx[d];
+				b_y -= dy[d];
+				break;
+				
+			} 
+			b_cnt++;
+			if(map[b_x][b_y] == 'O') {
+				break;
+			}
+		}
+		
+		return new int[]{r_x, r_y, b_x, b_y, r_cnt, b_cnt};
 	}
 	
 	 
