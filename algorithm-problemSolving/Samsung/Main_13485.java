31f361a6d07a418c288f9d8ea5727bb6849f8c83
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-04-08 15:46:08 +0900

    BOJ - 시험 감독(13485번) - 구현

---
+++ algorithm-problemSolving/Samsung/Main_13485.java
@@ -0,0 +1,36 @@
+// BOJ - 시험 감독(13485번)
+// 구현
+
+
+import java.util.Scanner;
+
+public class Main_13485 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			int num = scan.nextInt();
+			arr[i] = num;
+		}
+		
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		long ans = 0;
+		for(int i=0;i<n;i++) {
+			int cnt = 1;
+			int num = arr[i] - b;
+			if(num > 0) {
+				cnt += (num % c > 0?(num/c +1):(num/c));
+			}
+			ans += cnt;
+		}
+		
+		System.out.println(ans);
+
+	}
+
+}
