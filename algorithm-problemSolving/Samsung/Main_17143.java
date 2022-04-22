8257a5c2bbaa0d7a2149d96b634e8317b67ae1ac
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-04-22 19:17:11 +0900

    BOJ - 낚시왕(17143번) - 구현

--- algorithm-problemSolving/Samsung/Main_17143.java
+++ algorithm-problemSolving/Samsung/Main_17143.java
@@ -4,112 +4,110 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_17143 {
-	public static int R, C, M;
-	public static class Node {
+	public static int r, c, m;
+	public static Shark[][] shark;
+	
+	public static class Shark {
+		int x;
+		int y;
 		int s;
 		int d;
 		int z;
-		public Node(int s, int d, int z) {
+		public Shark(int x, int y, int s, int d, int z) {
+			this.x = x;
+			this.y = y;
 			this.s = s;
 			this.d = d;
 			this.z = z;
 		}
-
 	}
+	
 	public static int[] dx = {-1, 1, 0, 0};
 	public static int[] dy = {0, 0, 1, -1};
-	public static Node[][] map;
-
 	
-
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		int ans = 0;
-		map = new Node[R][C];
-
-		if(M==0) {
-			System.out.println(ans);
-			System.exit(0);
-		}
-		for(int i=0;i<M;i++) {
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		shark = new Shark[r][c];
+		
+		for(int i=0;i<m;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int r = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			map[r-1][c-1] = new Node(s, d-1, z);
+			shark[x-1][y-1] = new Shark(x-1, y-1, s, d-1, z);
 		}
 		
-		for(int j=0;j<C;j++) {
-			// 낚시~_~
-			for(int i=0;i<R;i++) {
-				if(map[i][j] != null) {
-					Node node = map[i][j];
-					ans += node.z;
-					map[i][j] = null;
+		int ans = 0;
+		for(int j=0;j<c;j++) {
+			for(int i=0;i<r;i++) {
+		
+				// 낚시
+				if(shark[i][j] != null) {
+					ans += shark[i][j].z;
+					shark[i][j] = null;
 					break;
 				}
 			}
 			
-			Node[][] map_copy = new Node[R][C];
+			Shark[][] shark_copy = new Shark[r][c];
 			
-			for(int x=0;x<R;x++) {
-				for(int y=0;y<C;y++) {
-					if(map[x][y] != null) {
-						Node node = map[x][y];
-						int nx = x;
-						int ny = y;
-						int move_cnt = node.s;
-						int dir = node.d;
-						while(move_cnt > 0) {
+			// 상어의 이동
+			for(int x=0;x<r;x++) {
+				for(int y=0;y<c;y++) {
+					if(shark[x][y] == null) continue;
+					Shark sk = shark[x][y];
+					int moving = 0;
+					if(sk.d <= 1) {
+						moving = (sk.s) % (r+r-2);
+					} else {
+						moving = (sk.s) % (c+c-2);
+					}
+					
+					int nx = sk.x;
+					int ny = sk.y;
+					int dir = sk.d;
+					while(moving-- > 0) {
+						nx += dx[dir];
+						ny += dy[dir];
+						if(nx < 0 || nx >= r || ny < 0 || ny >= c) {
+							nx -= dx[dir];
+							ny -= dy[dir];
+							if(dir % 2 == 0) {
+								dir += 1;
+							} else {
+								dir -= 1;
+							}
 							nx += dx[dir];
 							ny += dy[dir];
-							if(nx < 0 || nx >= R || ny < 0 || ny >= C) {
-								if(dir % 2 == 0) {
-									dir += 1;
-								} else {
-									dir -= 1;
-								}
-								nx += dx[dir];
-								ny += dy[dir];
-								continue;
-							}
-							move_cnt--;
 						}
-						
-						if(map_copy[nx][ny] != null) {
-							Node pre = map_copy[nx][ny];
-
-							if(pre.z < node.z) {
-								map_copy[nx][ny] = new Node(node.s, dir, node.z);
-
-							} 
-						} else {
-							map_copy[nx][ny] = new Node(node.s, dir, node.z);
+					}
+					
+					if(shark_copy[nx][ny] != null) {
+						if(shark_copy[nx][ny].z < sk.z) {
+							shark_copy[nx][ny] = new Shark(nx, ny, sk.s, dir, sk.z);
 						}
-						
+					} else {
+						shark_copy[nx][ny] = new Shark(nx, ny, sk.s, dir, sk.z);
 					}
 				}
 			}
-			map = map_copy;
-
+			shark = shark_copy;
 			
 		}
 		
 		System.out.println(ans);
-		
+
 	}
+	
 
 }
