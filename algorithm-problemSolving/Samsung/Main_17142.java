0cd8a4d7c17cb322ef803a674afbfb548e510cfe
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-04-23 01:19:42 +0900

    BOJ - 연구소3(17142번) - DFS + BFS

--- algorithm-problemSolving/Samsung/Main_17142.java
+++ algorithm-problemSolving/Samsung/Main_17142.java
@@ -9,7 +9,7 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_17142 {
-	public static int n, m, ans;
+	public static int n, m, ans, zero_cnt;
 	public static int[][] map;
 	public static int[] dx = {-1, 1, 0, 0};
 	public static int[] dy = {0, 0, -1, 1};
@@ -20,6 +20,12 @@ public class Main_17142 {
 		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
+		}
+		
+		public Node(int x, int y, int dis) {
+			this.x = x;
+			this.y = y;
+			this.dis = dis;
 		}
 
 	}
@@ -33,13 +39,15 @@ public class Main_17142 {
 		map = new int[n][n];
 		ans = Integer.MAX_VALUE;
 		virus = new ArrayList<>();
-
+		zero_cnt = 0;
 		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for(int j=0;j<n;j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if(map[i][j] == 2) {
 					virus.add(new Node(i, j));
+				} else if(map[i][j] == 0) {
+					zero_cnt++;
 				}
 
 			}
@@ -72,44 +80,42 @@ public class Main_17142 {
 	
 	public static int bfs(int[] arr) {
 		Queue<Node> q = new LinkedList<>();
-		int[][] visited = new int[n][n];
+		boolean[][] visited = new boolean[n][n];
 		for(int a:arr) {
 			Node node = virus.get(a);
-			visited[node.x][node.y] = 1;
+			node.dis = 0;
+			visited[node.x][node.y] = true;
 			q.add(node);
 		}
-		
+		int time = 0;
+		int cnt = 0;
 		while(!q.isEmpty()) {
 			Node node = q.poll();
 			for(int d=0;d<4;d++) {
 				int nx = node.x + dx[d];
 				int ny = node.y + dy[d];
 				if(0 <= nx && nx < n && 0 <= ny && ny < n) {
-				if(visited[nx][ny] == 0) {
-						if(map[nx][ny] != 1) {
-							visited[nx][ny] = visited[node.x][node.y] + 1;
-							q.add(new Node(nx, ny));
-						} 
+				if(!visited[nx][ny]) {
+					if(map[nx][ny] == 0) {
+						visited[nx][ny] = true;
+						cnt++;
+						time = Math.max(time, node.dis+1);
+						q.add(new Node(nx, ny, node.dis+1));
+					} else if(map[nx][ny] == 2){
+						visited[nx][ny] = true;
+						q.add(new Node(nx, ny, node.dis+1));
+					}
 				}
 			}
 			}
 		}
 		
-		int cnt = 1;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				if(map[i][j] == 0 && visited[i][j] == 0) {
-					return -1;
-				} 
-				if(map[i][j] == 0) {
-					cnt = Math.max(cnt, visited[i][j]);
-				}
-				
-			}
-		}
-		return cnt-1;
+		if(cnt != zero_cnt) return -1;
+		
+		return time;
 		
 		
 	}
 
 }
+
