fdd3e42ba60948ee5886941ec199d1240a95d023
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-06-02 17:00:17 +0900

    BOJ - 인내의 도미노 장인 호석(20165번) - BFS

--- algorithm-problemSolving/dfs&bfs/BOJ/Main_20165.java
+++ algorithm-problemSolving/dfs&bfs/BOJ/Main_20165.java
@@ -1,118 +1,109 @@
-// 푸는 중
+// BOJ - 인내의 도미노 장인 호석(20165번)
+// BFS
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_20165 {
-
-	static int[][] map, temp;
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = { 0, 1, 0, -1};
-	static int answer;
-	static int N, M, R;
+	public static int n, m, r, score;
+	// 동, 서, 남, 북
+	public static int[] dx = {0, 0, 1, -1};
+	public static int[] dy = {1, -1, 0, 0};
+	public static boolean[][] visited;
+	public static int[][] map;
 	
-	public static void main(String[] args) throws IOException {
+	public static class Node {
+		int x;
+		int y;
+		int dir;
 		
+		public Node(int x, int y, int dir) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+		}
+	}
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		map = new int[n+1][m+1];
+		visited = new boolean[n+1][m+1];
 		
-		st = new StringTokenizer(br.readLine()," ");
 		
-		N = Integer.parseInt(st.nextToken()); // 행
-		M = Integer.parseInt(st.nextToken()); // 열
-		R = Integer.parseInt(st.nextToken()); // 라운드 수 
-		
-		map = new int[N][M];
-		temp = new int[N][M];
-		
-		for( int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			for( int j=0; j<M; j++) {
+		for(int i=1;i<=n;i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=1;j<=m;j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for( int i=0; i<N; i++) {
-			for( int j=0; j<M; j++) {
-				temp[i][j] = map[i][j];
-			}
+		
+		for(int i=0;i<r;i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			char dir = st.nextToken().charAt(0);
+			if(!visited[x][y])
+				bfs(x, y, dir);
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			visited[x][y] = false;
+			
 		}
 		
-		
-		for( int r=0; r<R; r++) {
-			// 공격하기
-			st = new StringTokenizer(br.readLine()," ");
-			int Ar = Integer.parseInt(st.nextToken())-1;
-			int Ac = Integer.parseInt(st.nextToken())-1;
-			char direct = st.nextToken().charAt(0);
-			
-			Attack(Ar,Ac,direct);
-			
-			// 수비하기
-			st = new StringTokenizer(br.readLine()," ");
-			int Dr = Integer.parseInt(st.nextToken())-1;
-			int Dc = Integer.parseInt(st.nextToken())-1;
-			int t = temp[Dr][Dc];
-			map[Dr][Dc] = t;
-		}
-		
-		System.out.println(answer);
-		for( int[] output : map) {
-			for( int out : output) {
-				System.out.print(out>=1 ? "S " : "F ");
+		System.out.println(score);
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=m;j++) {
+				if(visited[i][j]) {
+					System.out.print("F ");
+				} else {
+					System.out.print("S ");		
+				}
 			}
 			System.out.println();
 		}
-	}
-	private static void Attack(int r, int c, char direct) {
-		// 특이 사항
-		// 1. 이미 넘어진 도미노가 있는 칸을 공격한 경우 아무일도 안 일어난다.
-		// 2. 넘어지지 않은 도미노 세우려면 아무일도 안일어난다.
-		
-		// 방향
-		int direction =0;
-		switch( direct) {
-		case 'E' : 
-			direction = 1;
-			break;
-		case 'W' :
-			direction = 3;
-			break;
-		case 'S' :
-			direction = 2;
-			break;
-		case 'N' :
-			direction = 0;
-			break;
-		default:
-			break;
-		}
-		int height = map[r][c]; // 해당 도미노의 높이 
-		int nr = r;
-		int nc = c;
-		while( height > 0 ) {
-			// 세워져 있다면
-			if ( map[nr][nc] > 0 ) {
-				height = Math.max(height, map[nr][nc]);
-				height --;
-				map[nr][nc] =0 ; // 쓰러뜨리기
-				answer ++;
 
+	}
+	
+	public static void bfs(int x, int y, char dir) {
+		Queue<Node> q = new LinkedList<>();
+
+		int d = 0;
+		if(dir == 'E') d = 0;
+		else if(dir == 'W') d = 1;
+		else if(dir == 'S') d = 2;
+		else d = 3;
+		
+		q.add(new Node(x, y, d));
+		score++;
+		visited[x][y] = true;
+		
+		while(!q.isEmpty()) {
+			Node node = q.poll();
+			int height = map[node.x][node.y];
+			for(int i=1;i<height;i++) {
+				int nx = node.x + dx[node.dir]*i;
+				int ny = node.y + dy[node.dir]*i;
+				if(nx < 1 || nx > n || ny < 1 || ny > m) continue;
+				if(!visited[nx][ny]) {
+
+					visited[nx][ny] = true;
+					score++;
+					q.add(new Node(nx, ny, node.dir));
+				}
 			}
-			// 눕혀져 있더라도
-			else {
-				height --;
-			}
-			nr = nr + dr[direction];
-			nc = nc + dc[direction];
 			
-			if ( !isIn(nr,nc)) break;
 		}
+	}
+	
 
-	}
-	private static boolean isIn(int r , int c ) {
-		if ( r>=0 && r<N && c>=0 && c<M) return true;
-		
-		return false;
-	}
+
 }
+
