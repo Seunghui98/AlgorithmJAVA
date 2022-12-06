85254ff6a5f3ee309da156dbaeb9d61cf4e8d693
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-12-06 23:31:38 +0900

    BOJ - 거짓말(1043) - Union Find

--- 1day1algo/1211/1206.java
+++ 1day1algo/1211/1206.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
+
 public class Main_1043 {
     public static int[] parent;
     public static void main(String[] args) throws IOException {
